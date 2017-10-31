6481b343c8339b0c2b7863161652019d37428b69
Michal Kimle <kimle.michal@gmail.com>
2017-10-31 14:51:22 +0100

    Packaging script cleanup + version selection

--- packaging.sh
+++ packaging.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 
-export PATH=$HOME_DIR/go/bin:$PATH
-export GOPATH=$HOME_DIR/gopath 
-TELEGRAF_PATH=$GOPATH/src/github.com/influxdata/telegraf
+export PATH=${HOME_DIR}/go/bin:$PATH
+export GOPATH=${HOME_DIR}/gopath
+TELEGRAF_PATH=${GOPATH}/src/github.com/influxdata/telegraf
 
-git clone $REPOSITORY $TELEGRAF_PATH
+git clone ${REPOSITORY} ${TELEGRAF_PATH}
 
-cd $TELEGRAF_PATH
-git checkout $BRANCH
+cd ${TELEGRAF_PATH}
+git checkout ${BRANCH}
+RELEASE=$(git describe --tags)
 
-$TELEGRAF_PATH/scripts/build.py --package --platform=linux --arch=amd64
+${TELEGRAF_PATH}/scripts/build.py --package --platform=linux --arch=amd64 --version=${RELEASE}
 
-cp $TELEGRAF_PATH/build/* $HOME_DIR/build/ 
+cp ${TELEGRAF_PATH}/build/* ${HOME_DIR}/build/
